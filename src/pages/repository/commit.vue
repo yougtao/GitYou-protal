ade0415ce608355af19cdca565ace770d6cc8013
yongtao <cytcyt123@163.com>
2019-10-23 18:22:12 +0800

    feature: commit页文件对比

--- src/pages/repository/commit.vue
+++ src/pages/repository/commit.vue
@@ -7,23 +7,42 @@
     <div class="content-files">
       <div class="content-file" v-for="file in fileList" :key="file.path">
         <div class="file-title">
-          <i @click="showFile(file.path)" class="el-icon-arrow-right"></i>
-          <span class="file-name">{{file.name}}</span>
+          <i @click="loadFile(file)" class="el-icon-arrow-right"></i>
+          <span @click="loadFile(file)" class="file-name">{{file.name}}({{file.type}})</span>
           <span class="file-status">
-            <span class="status-add">{{file.insertions}}+</span>
-            <span class="status-del">{{file.deletions}}-</span>
+            <span class="status-add">+{{file.insertions}}&nbsp</span>
+            <span class="status-del">-{{file.deletions}}&nbsp</span>
           </span>
           <span class="file-path">
             <a @click="toFile(file.path)" href="javascript:void(0)">{{file.path}}</a>
           </span>
         </div>
-        <div class="file-body"></div>
+        <div v-show="file.show" class="file-body">
+          <table v-if="file.diffLines !==undefined" class="diff-table">
+            <tbody>
+            <tr class="diff-first">
+              <td class="diff-num"></td>
+              <td class="diff-num"></td>
+              <td class="diff-text">{{file.statistics}}</td>
+            </tr>
+            <tr v-for="(line,index) in file.diffLines" :class="getClass(line.sign)">
+              <td class="diff-num">{{line.num1 === 0 ? '':line.num1}}</td>
+              <td class="diff-num">{{line.num2 === 0 ? '':line.num2}}</td>
+              <td class="diff-text">
+                <span class="diff-text-sign">&nbsp{{line.sign}}&nbsp</span><span>{{line.text}}</span>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: 'commit',
   data() {
@@ -55,13 +74,72 @@ export default {
         }
       }).then(({data}) => {
         this.fileList = data
+        for (let i = 0; i < this.fileList.length; i++) {
+          Vue.set(this.fileList[i], 'show', true)
+        }
       })
+    },
+    /* 文件MODIFY */
+    fileDiff(file) {
+      this.$http.get('/repo/commit/diff', {
+        params: {
+          user: this.repository.user,
+          name: this.repository.name,
+          commit: this.repository.commitId,
+          path: file.path
+        }
+      }).then(({data}) => {
+        let diffLines = []
+
+        let lines = data.diff.split(/\n/)
+        let index1 = 0
+        let index2 = 0
+        for (let i = 0; i < lines.length; i++) {
+          if (lines[i] === '')
+            break
+          let line = {}
+          line.sign = lines[i].substring(0, 1)
+          line.text = lines[i].substring(1)
+          if (line.sign === '-') {
+            line.num1 = ++index1
+            line.num2 = 0
+          } else if (line.sign === '+') {
+            line.num1 = 0
+            line.num2 = ++index2
+          } else if (line.sign === ' ') {
+            line.num1 = ++index1
+            line.num2 = ++index2
+          } else if (line.sign === '\\') {
+            line.num1 = 0
+            line.num2 = 0
+          }
+          diffLines.push(line)
+        }
+        Vue.set(file, 'diffLines', diffLines)
+        file.statistics = data.statistics
+        console.log(this.fileList)
+      })
+    },
+    loadFile(file) {
+      if (file.diffLines === undefined) {
+        this.fileDiff(file)
+      } else {
+        file.show = !file.show
+      }
+    },
+    getClass(sign) {
+      if (sign === '-') {
+        return 'diff-line-delete'
+      } else if (sign === '+') {
+        return 'diff-line-add'
+      } else if (sign === '\\') {
+        return 'diff-line-split'
+      }
     },
     toFile(path) {
       const repo = '/' + this.repository.user + '/' + this.repository.name
       this.$router.push(repo + '/blob/' + this.repository.curBranch + '/' + path)
-    },
-    showFile(path) {}
+    }
   },
   computed: {
     addNum() {
@@ -112,11 +190,71 @@ a {
 
 .file-path {
   margin-left: 16px;
-  font-size: 13px;
+  font-weight: 500;
+  font-size: 14px;
 }
 
 .file-path a:hover {
   text-decoration-line: underline;
   color: #0366d6;
 }
+
+/* diff table */
+.diff-table {
+  border-collapse: collapse;
+}
+
+.diff-table td {
+  padding: 4px 10px;
+  text-align: left;
+}
+
+.diff-first td {
+  background-color: #dbedff;
+  color: #767B81;
+  font-size: 14px;
+  user-select: none;
+}
+
+.diff-first td:last-child {
+  background-color: #f1f8ff;
+}
+
+.diff-num {
+  min-width: 40px;
+  color: #586069;
+  text-align: center;
+  user-select: none;
+}
+
+.diff-text {
+  width: 100%;
+}
+
+.diff-text-sign {
+  user-select: none;
+}
+
+.diff-line-add td {
+  background-color: #cdffd8;
+}
+
+.diff-line-add .diff-text {
+  background-color: #e6ffed;
+}
+
+.diff-line-delete td {
+  background-color: #ffdce0;
+}
+
+.diff-line-delete .diff-text {
+  background-color: #ffeef0;
+}
+
+.diff-line-split {
+  height: 20px;
+  color: #ccc;
+  font-size: 14px;
+}
+
 </style>
