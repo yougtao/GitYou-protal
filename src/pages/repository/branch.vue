a20670c3b111f92ed9d8af4d36ad0c9b94aa831f
yongtao <cytcyt123@163.com>
2019-11-07 17:27:01 +0800

    perfect: branch页面, 将当前branch'打钩'

--- src/pages/repository/branch.vue
+++ src/pages/repository/branch.vue
@@ -22,7 +22,13 @@
                 </div>
               </div>
               <ul class="dropmenu-list">
-                <li class="selected" @click="curBranch = branch.name" v-for="branch in branches" :key="branch.name">{{branch.name}}</li>
+                <li :class="curBranch === branch.name ? 'selected':''" @click="curBranch = branch.name" v-for="branch in branches"
+                    :key="branch.name">
+                  <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
+                  </svg>
+                  <span>{{branch.name}}</span>
+                </li>
               </ul>
             </div>
           </div>
